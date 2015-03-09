99c43efa192c474e13d45d0cbda62a6e7c59ea5b
mcriqui <maggie.criqui@gmail.com>
2015-03-09 10:54:50 -0400

    tried to get urls to work

--- bracket/urls.py
+++ bracket/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from bracket import views
 
+
 urlpatterns = patterns('',
 
 url(r'^admin/', include(admin.site.urls)),
